62cb0c1e73f842776e18d033e78c53c4acb8c646
updowncode <updowncode@gmail.com>
2018-03-31 15:17:44 -0400

    flexbox

--- src/Hello1.tsx
+++ src/Hello1.tsx
@@ -20,20 +20,47 @@ export default class HelloWorldApp extends Component<IHelloFormProps, any> {
   render() {
     let display = this.state.showText ? this.props.name : "Eric";
     return (
-      <View style={{flex: 1, height:100, backgroundColor: 'blue'}}>
-        <Text style={ styles.red}>Hello {this.props.name}</Text>
-        <Text style={styles.bigblue}>Hello {display}</Text>
+      <View>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            //justifyContent: "flex-start",
+            //alignItems: "stretch",
+           // height: 50,
+            backgroundColor: "blue"
+          }}
+        >
+          <Text style={styles.red}>Hello {this.props.name}</Text>
+          <Text style={styles.bigblue}>Hello {display}</Text>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "center"
+            //alignItems: 'center',
+          }}
+        >
+          <View
+            style={{ width: 50, height: 50, backgroundColor: "powderblue" }}
+          />
+          <View style={{ width: 50, height: 50, backgroundColor: "skyblue" }} />
+          <View
+            style={{ width: 50, height: 50, backgroundColor: "steelblue" }}
+          />
+        </View>
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
-    bigblue: {
-      color: '#19d5db',
-      fontWeight: 'bold',
-      fontSize: 30,
-    },
-    red: {
-      color: 'red',
-    },
-  });
+  bigblue: {
+    color: "#19d5db",
+    fontWeight: "bold",
+    fontSize: 30
+  },
+  red: {
+    color: "red"
+  }
+});
