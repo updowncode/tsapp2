91494fe044c125e882e7dd1212e22add8e9c9395
updowncode <updowncode@gmail.com>
2018-04-02 03:11:41 -0400

    input

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  *
- * Generated with the TypeScript template
+ * rated with the TypeScript template
  * https://github.com/emin93/react-native-template-typescript
  */
 
